1369e3e120a629fe99bb1ab51cc2e50ec6ed49f5
nezlicodes <benmeradi.lilyanezli@gmail.com>
2019-12-04 22:19:53 +0100

    WIP

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { MatExpansionModule } from "@angular/material/expansion";
 import { MatSelectModule } from "@angular/material/select";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import {MatInputModule } from "@angular/material/input";
+import {  MatSidenavModule } from "@angular/material/sidenav";
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TaskListComponent } from './components/task-list/task-list.component';
@@ -45,7 +47,9 @@ import { CreateTaskComponent } from './components/create-task/create-task.compon
     MatMenuModule,
     MatFormFieldModule,
     MatSelectModule,
-    MatInputModule
+    MatSidenavModule,
+    MatInputModule, 
+    FlexLayoutModule
   ],
   providers: [MessageService, HttpErrorHandlerService],
   bootstrap: [AppComponent]
